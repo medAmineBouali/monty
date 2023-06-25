637afa1c036b118caca77347464aff3afaedcc2d
amine1 <medaminebouali1@gmail.com>
2023-06-25 21:15:50 +0000

    final

--- operations.c
+++ operations.c
@@ -98,39 +98,3 @@ void f_pop(stack_t **head, unsigned int counter)
 	*head = h->next;
 	free(h);
 }
-/**
- * f_push - add node to the stack
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_push(stack_t **head, unsigned int counter)
-{
-	int n, j = 0, flag = 0;
-
-	if (info.arg)
-	{
-		if (info.arg[0] == '-')
-			j++;
-		for (; info.arg[j] != '\0'; j++)
-		{
-			if (info.arg[j] > 57 || info.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(info.file);
-			free(info.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(info.file);
-		free(info.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	n = atoi(info.arg);
-	if (info.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
-}
